427829ed74236e191310806f8f9a9e1aea9fadef
Rustie Lin <rustielin@berkeley.edu>
2020-01-19 02:15:30 +0000

    hacky payments working xd

--- ilp/index.js
+++ ilp/index.js
@@ -20,5 +20,19 @@ app.get('/testpay', (req, res) => {
 //  run().catch(e => console.error(e))
 })
 
+app.get('/pay', (req, res) => {
+  if (typeof req.query.nonprofit === 'undefined' || typeof req.query.amount === 'undefined') {
+    res.send('Request malformed. Specify \"nonprofit\" and \"amount\"')
+    return
+  } 
+  console.log('paying', req.query.nonprofit, req.query.amount)
+    SPSP.pay(plugin, {
+      receiver: req.query.nonprofit,
+      sourceAmount: req.query.amount
+    })
+  console.log('paid!')
+  res.send('Paid', req.query.nonprofit, req.query.amount)
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
