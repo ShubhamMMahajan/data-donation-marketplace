b43047a838e6fb86f06d81d21a9be68006a4b858
Shubham Mahajan <shubhammahajan@gmail.com>
2020-01-18 22:47:21 -0800

    Updated api

--- gcp_flask_app/main.py
+++ gcp_flask_app/main.py
@@ -1,25 +1,32 @@
 from flask import Flask, request, render_template,jsonify
+import json
 app = Flask(__name__)
-def do_something(text1,text2):
-   text1 = text1.upper()
-   text2 = text2.upper()
-   combine = text1 + text2
-   return combine
+listings = []
+constant_price = 5
+#description = "records how long your phone is in the case "
+def add_listing(blockstack_id, name, about):
+    new_listing = dict()
+    new_listing['blockstack_id'] = blockstack_id 
+    new_listing['price'] = constant_price
+    new_listing['about'] = about
+    new_listing['name'] = name
+    listings.append(new_listing)
+
+@app.route('/get_data', methods=['PUT'])
+def create_task():
+    #blockstack_id = request.args.get('blockstack_id', "")
+    blockstack_id = request.form['blockstack_id']
+    name = request.form['name']
+    about = request.form['about']
+    add_listing(blockstack_id, name, about)
+    return str(request.args)
+    #return render_template('index.html', ids=listings)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', ids=listings)
 
-@app.route('/join', methods=['GET','POST'])
-def my_form_post():
-    text1 = request.form['text1']
-    word = request.args.get('text1')
-    text2 = request.form['text2']
-    combine = do_something(text1,text2)
-    result = {
-        "output": combine
-    }
-    result = {str(key): value for key, value in result.items()}
-    return jsonify(result=result)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
